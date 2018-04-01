b096bd4f689922522a70ef89522ec4c0a8a21c5b
Zohaib Shahzad <zohaibshahzad.to@gmail.com>
2018-03-31 20:48:01 -0400

    Application completed + Review
    See ReadME
    - Review of application and Redux
    - Added more book detail
    - implemented condition where if there is not initial book selected, it would return a line telling us to do so. Its a counter for the null in the beginning.

--- src/reducers/reducers_book.js
+++ src/reducers/reducers_book.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'Javascript: The Good Parts' },
-    { title: 'Harry Potter' },
-    { title: 'The Dark Power' },
-    { title: 'Eloquent Ruby' }
+    { title: 'Javascript: The Good Parts', pages: 101 },
+    { title: 'Harry Potter', pages: 39 },
+    { title: 'The Dark Power', pages: 85 },
+    { title: 'Eloquent Ruby', pages: 1 }
   ];
 }
